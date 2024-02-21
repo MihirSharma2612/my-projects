1e2fdd3a2d9d35a41e4f2d49b812acfa0cc46f91
MihirSharma2612 <mihirsharmamihirsharma@3gmail.com>
2024-02-21 23:27:12 +0530

    first comment

---
+++ app.js
@@ -0,0 +1,16 @@
+//console.log("Mihir sharma");
+
+
+//console.dir(document.querySelector(".navbar"));
+
+//console.dir(document.querySelector("h3"));
+
+//document.addEventListener("DOMContentLoaded", function() {
+//    console.dir(document.querySelector(".navbar"));
+//});
+
+console.dir(document.querySelector(".navbar"));
+
+document.querySelectorAll("p");
+
+document.getElementsByClassName("navbar");
---
+++ readme.md
@@ -0,0 +1,2 @@
+# This is my html ,css, js website
+ for anything you can contact me = mihirsharmamihirsharma3@gmail.com
